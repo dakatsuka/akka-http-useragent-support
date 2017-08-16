931b84c9d0f59b4e05aec0a0492f7f7cec4ef814
Dai Akatsuka <d.akatsuka@gmail.com>
2017-08-17 04:47:13 +0900

    Add specs

---
+++ src/test/scala/jp/dakatsuka/akka/http/useragent/UserAgentSpec.scala
@@ -0,0 +1,22 @@
+package jp.dakatsuka.akka.http.useragent
+
+import org.scalatest.{ DiagrammedAssertions, FlatSpec }
+
+class UserAgentSpec extends FlatSpec with DiagrammedAssertions {
+  behavior of "UserAgent"
+
+  "#toString" should "return raw header string" in {
+    val uaString = "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko"
+    val ua       = UserAgent(uaString)
+
+    assert(ua.toString == uaString)
+  }
+
+  "#isClawler" should "check if UserAgent is clawler" in {
+    val uaIsClawler    = UserAgent("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")
+    val uaIsNotClawler = UserAgent("Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko")
+
+    assert(uaIsClawler.isCrawler)
+    assert(!uaIsNotClawler.isCrawler)
+  }
+}
---
+++ src/test/scala/jp/dakatsuka/akka/http/useragent/UserAgentSupportSpec.scala
@@ -0,0 +1,64 @@
+package jp.dakatsuka.akka.http.useragent
+
+import akka.http.javadsl.server.MissingHeaderRejection
+import akka.http.scaladsl.model.headers.`User-Agent`
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import org.scalatest.{ DiagrammedAssertions, FlatSpec }
+
+class UserAgentSupportSpec extends FlatSpec with ScalatestRouteTest with DiagrammedAssertions {
+  import UserAgentSupport._
+
+  behavior of "UserAgentSupport"
+
+  "#extractUserAgent" should "provide UserAgent" in {
+    val route = extractUserAgent { ua =>
+      complete(OK -> ua.toString)
+    }
+
+    val header = "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko"
+
+    Get("/").addHeader(`User-Agent`(header)) ~> route ~> check {
+      assert(status == OK)
+      assert(responseAs[String] == header)
+    }
+  }
+
+  it should "reject when User-Agent header not found " in {
+    val route = extractUserAgent { ua =>
+      complete(OK -> ua.toString)
+    }
+
+    Get("/") ~> route ~> check {
+      assert(!handled)
+      assert(rejection.isInstanceOf[MissingHeaderRejection])
+    }
+  }
+
+  "#optionalUserAgent" should "provide Option[UserAgent]" in {
+    val route = optionalUserAgent {
+      case Some(ua) => complete(OK -> ua.toString)
+      case None     => complete(OK -> "")
+    }
+
+    val header = "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko"
+
+    Get("/").addHeader(`User-Agent`(header)) ~> route ~> check {
+      assert(status == OK)
+      assert(responseAs[String] == header)
+    }
+  }
+
+  it should "provide None when User-Agent header not found" in {
+    val route = optionalUserAgent {
+      case Some(ua) => complete(OK -> ua.toString)
+      case None     => complete(OK -> "")
+    }
+
+    Get("/") ~> route ~> check {
+      assert(status == OK)
+      assert(responseAs[String] == "")
+    }
+  }
+}
