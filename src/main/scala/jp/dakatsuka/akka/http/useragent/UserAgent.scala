31537e0f05cdf42e599c161e32944aeb9f46230f
Dai Akatsuka <d.akatsuka@gmail.com>
2017-08-17 03:37:36 +0900

    Apply scalafmt

--- src/main/scala/jp/dakatsuka/akka/http/useragent/UserAgent.scala
+++ src/main/scala/jp/dakatsuka/akka/http/useragent/UserAgent.scala
@@ -3,14 +3,14 @@ package jp.dakatsuka.akka.http.useragent
 import is.tagomor.woothee.Classifier
 
 case class UserAgent(
-  raw: String,
-  name: String,
-  category: String,
-  os: String,
-  version: String,
-  osVersion: Option[String],
-  vendor: Option[String]
+    raw: String,
+    name: String,
+    category: String,
+    os: String,
+    version: String,
+    osVersion: Option[String],
+    vendor: Option[String]
 ) {
   override def toString: String = raw
-  def isCrawler: Boolean = Classifier.isCrawler(raw)
+  def isCrawler: Boolean        = Classifier.isCrawler(raw)
 }
